25b1b4957168f399940f414bd80fccbe85680b4c
alokrk <alok.kucheria@gmail.com>
2016-02-26 04:36:36 -0500

    Complete data extraction

--- tags.R
+++ tags.R
@@ -2,11 +2,52 @@
 setwd("/media/alok/New Volume/sodata")
 require("XML")
 
-data = xmlParse("Tags.xml")
-tags = xmlToList(data)
-
-len = length(tags)
-#print(len)
+#TAGS
+tags = xmlParse("Tags.xml")
+tags = xmlToList(tags)
+lenTags = length(tags)
 
 #df = data.frame(matrix(unlist(tags)), nrow = len)
-df = data.frame(Reduce(rbind, tags))
+tags = data.frame(Reduce(rbind, tags))
+
+#POST LINKS
+postLinks = xmlParse("PostLinks.xml")
+postLinks = xmlToList(postLinks)
+lenpostLinks = length(postLinks)
+
+postLinks = data.frame(Reduce(rbind, postLinks))
+
+#USERS
+users = xmlParse("Users.xml")
+users = xmlToList(users)
+lenUsers = length(users)
+
+users = data.frame(Reduce(rbind, users))
+
+#BADGES
+badges = xmlParse("Badges.xml")
+badges = xmlToList(badges)
+lenBadges = length(badges)
+
+badges = data.frame(Reduce(rbind, badges))
+
+#VOTES
+votes = xmlParse("Votes.xml")
+votes = xmlToList(votes)
+lenVotes = length(votes)
+
+votes = data.frame(Reduce(rbind, votes))
+
+#COMMENTS
+comments = xmlParse("Comments.xml")
+comments = xmlToList(comments)
+lenComments = length(comments)
+
+comments = data.frame(Reduce(rbind, comments))
+
+#POSTS
+posts = xmlParse("Posts.xml")
+posts = xmlToList(posts)
+lenPosts = length(posts)
+
+posts = data.frame(Reduce(rbind, posts))
