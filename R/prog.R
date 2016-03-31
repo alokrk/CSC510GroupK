8374379ac513572d7e8b8088bc9249bcd55f217c
alokrk <alok.kucheria@gmail.com>
2016-03-30 22:00:01 -0400

    Covert data to CSV

--- R/prog.R
+++ R/prog.R
@@ -10,12 +10,17 @@ lenTags = length(tags)
 #df = data.frame(matrix(unlist(tags)), nrow = len)
 tags = data.frame(Reduce(rbind, tags))
 
+
+write.csv(tags, file="tags.csv")
+
 #POST LINKS
 postLinks = xmlParse("PostLinks.xml")
 postLinks = xmlToList(postLinks)
 lenpostLinks = length(postLinks)
 
 postLinks = data.frame(Reduce(rbind, postLinks))
+
+write.csv(postLinks, file="postLinks.csv")
 
 #USERS
 users = xmlParse("Users.xml")
@@ -24,12 +29,16 @@ lenUsers = length(users)
 
 users = data.frame(Reduce(rbind, users))
 
+write.csv(users, file="users.csv")
+
 #BADGES
 badges = xmlParse("Badges.xml")
 badges = xmlToList(badges)
 lenBadges = length(badges)
 
 badges = data.frame(Reduce(rbind, badges))
+
+write.csv(badges, file="badges.csv")
 
 #VOTES
 votes = xmlParse("Votes.xml")
@@ -38,12 +47,16 @@ lenVotes = length(votes)
 
 votes = data.frame(Reduce(rbind, votes))
 
+write.csv(votes, file="votes.csv")
+
 #COMMENTS
 comments = xmlParse("Comments.xml")
 comments = xmlToList(comments)
 lenComments = length(comments)
 
 comments = data.frame(Reduce(rbind, comments))
+
+write.csv(comments, file="comments.csv")
 
 #POSTS
 posts = xmlParse("Posts.xml")
@@ -52,9 +65,13 @@ lenPosts = length(posts)
 
 posts = data.frame(Reduce(rbind, posts))
 
+write.csv(posts, file="posts.csv")
+
 #POST HISTORY
 postHistory = xmlParse("PostHistory.xml")
 postHistory = xmlToList(postHistory)
 lenPostHistory = length(postHistory)
 
 postHistory = data.frame(Reduce(rbind, postHistory))
+
+write.csv(postHistory, file="postHistory.csv")
